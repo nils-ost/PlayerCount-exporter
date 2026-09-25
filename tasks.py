67827343b3f12621abf1cd406126d96d42740614
Nils Ost <home@nijos.de>
2026-09-25 23:33:09 +0200

    optimized container build/push invoker

--- tasks.py
+++ tasks.py
@@ -1,17 +1,21 @@
 from invoke import task
 
+image = 'nilsost/playercount-exporter'
 
-@task(name='build-image', aliases=['ib', ])
+
+@task(name='container-image-build', aliases=['cib', ])
 def build_container_image(c):
     version = c.run('git describe')
     version = version.stdout.strip().replace('v', '', 1).rsplit('-', 1)[0].replace('-', '.')
-    c.run(f'cd app; sudo docker buildx build --platform linux/arm64,linux/amd64,linux/arm/v7 -t nilsost/playercount-exporter:{version} .')
-    c.run('cd app; sudo docker buildx build --platform linux/arm64,linux/amd64,linux/arm/v7 -t nilsost/playercount-exporter:latest .')
+    tags = f' -t {image}:{version}'
+    tags += f' -t {image}:latest'
+    c.run(f'cd app; sudo docker buildx build --platform linux/arm64,linux/amd64,linux/arm/v7{tags} --load .')
 
 
-@task(name='build-push-image', aliases=['ibp', ])
+@task(name='container-image-push', aliases=['cip', ])
 def push_container_image(c):
     version = c.run('git describe')
     version = version.stdout.strip().replace('v', '', 1).rsplit('-', 1)[0].replace('-', '.')
-    c.run(f'cd app; sudo docker buildx build --platform linux/arm64,linux/amd64,linux/arm/v7 -t nilsost/playercount-exporter:{version} --push .')
-    c.run('cd app; sudo docker buildx build --platform linux/arm64,linux/amd64,linux/arm/v7 -t nilsost/playercount-exporter:latest --push .')
+    tags = f' -t {image}:{version}'
+    tags += f' -t {image}:latest'
+    c.run(f'cd app; sudo docker buildx build --platform linux/arm64,linux/amd64,linux/arm/v7{tags} --push .')
